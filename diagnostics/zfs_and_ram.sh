af508bd59b46316a97ef56ab0c32ab81922948a9
rivlid <karen52rus@yandex.ru>
2026-04-24 15:45:53 +0300

    zfs and ram edit

--- diagnostics/zfs_and_ram.sh
+++ diagnostics/zfs_and_ram.sh
@@ -8,4 +8,4 @@ echo "3. Свободно: $(free -h | awk '/^Mem:/ {print $4}')"
 echo "4. ARC size: $(cat /proc/spl/kstat/zfs/arcstats | awk '/^size/ {printf "%.1f", $3/1024/1024/1024}') GB"
 echo "5. Hit ratio: $(awk '/^hits/ {h=$3} /^misses/ {m=$3} END {printf "%.1f%%", h/(h+m)*100}' /proc/spl/kstat/zfs/arcstats)"
 echo "6. Количество ВМ: $(qm list | wc -l)"
-echo "7. Память всех ВМ: $(qm list | awk 'NR>1 {sum+=$4} END {printf "%.1f", sum/1024}') GB
+echo "7. Память всех ВМ: $(qm list | awk 'NR>1 {sum+=$4} END {printf "%.1f", sum/1024}') GB"
